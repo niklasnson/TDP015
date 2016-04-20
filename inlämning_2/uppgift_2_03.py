ad3872e14c45229f3f2aea033c4aa8e1f57a4753
Niklas Nson <niklasnson@me.com>
2016-04-20 16:15:03 +0200

    borde nu vara klar, inga duplicates hittar jag i alla fall

--- inlämning_2/uppgift_2_03.py
+++ inlämning_2/uppgift_2_03.py
@@ -1,15 +1,23 @@
 def subsets(s):
     items = [[(a + 1,b + 1) for a in range(max(s))] for b in range(max(s))]
-    items = flatten(items)
+    items = flatten_array(items)
+    items = remove_dublicates(items)
     for a in range(max(s)):
         items.append((a + 1,0))
     return items
 
-def flatten(items):
+def flatten_array(items):
     flat = []
     for p in items:
         for i in p:
             flat.append(i)
     return flat
 
-print(subsets((1,2,3)))
+def remove_dublicates(items):
+    clean = []
+    for i in items:
+        if (i[1], i[0]) not in clean:
+            clean.append(i)
+    return clean
+
+print(subsets((1,2)))
