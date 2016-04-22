1f7b9ed7b0327cb9b12a5d47a523cf8f2be1b6a8
Niklas Nson <niklasnson@me.com>
2016-04-22 05:40:30 +0200

    added new files

--- inlämning_2/uppgift_2_03.py
+++ inlämning_2/uppgift_2_03.py
@@ -1,14 +1,22 @@
 def subsets(s):
     t = []
+    spinner = []
     for el in s:
-        tmp = set()
-        tmp.add(el)
-        t.append(tmp)
-        for le in t:
-            print(len(t))
-    return t
+        tmp = set([el])
+        if not tmp in t:
+            t.append(tmp)
+            spinner.append(el)
+        for le in spinner:
+            print(le)
+            pmt = set([el,le])
+            if (le, el) not in t:
+                print((le,el))
+                if (el,le) not in t:
+                    print((el,le))
+                    t.append(pmt)
+    return sorted(t)
 # (1,2) => [(0), (1), (2), (1,2)]
 # (a,b) => [(0), (a), (b), (a,b)]
 # (1,2,3) => [(0), (1), (2), (3), (1,2), (1,3), (2,3), (1,2,3) ]
 
-print(subsets((1,2,3)))
+print(subsets((1,2,5)))
